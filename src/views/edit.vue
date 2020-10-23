255416ea6c29934e76366b346d8b2e63e021ff53
dengjuan <1182926231@qq.com>
2020-10-23 18:07:22 +0800

    裁剪功能完成

--- src/views/edit.vue
+++ src/views/edit.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="edit">
     <hm-headers>编辑资料</hm-headers>
+    <!-- 头像裁剪遮罩 -->
+    <div class="mask" v-show="showCropper">
+      <van-button class="cancle_btn" type="primary" v-show="showCropper" @click="showCropper = false">取消</van-button>
+      <van-button class="confirm_btn" type="info" v-show="showCropper" @click="cropper">确定</van-button>
+      <vueCropper
+        ref="cropper"
+        :canMove="true"
+        :fixed="true"
+        :auto-upload="false"
+        :autoCrop="true"
+        :img="option.img"
+        :autoCropWidth="150"
+        :autoCropHeight="150"
+        :outputSize="option.size"
+        :outputType="option.outputType"
+      />
+    </div>
     <!-- 头像 -->
     <div class="avator">
       <img :src="$axios.defaults.baseURL + userInfo.head_img" alt="" />
       <van-uploader :after-read="afterRead" class="uploader" />
-    </div>
-    <!-- 头像裁剪遮罩 -->
-    <div class="mask">
-      <van-button type="primary" @click="showCropper = false" v-show="showCropper">取消</van-button>
-      <van-button type="info" @click="cropper" v-show="showCropper">确定</van-button>
-      <van-overlay :show="showCropper" />
     </div>
     <hm-navbar title="昵称" :content="userInfo.nickname" @click="showNameDialog" />
     <!-- 修改昵称 -->
@@ -40,7 +51,6 @@
         </van-cell-group>
       </van-radio-group>
     </van-dialog>
-    <vueCropper ref="cropper" :auto-upload="false" :autoCrop="true" :img="option.img" :outputSize="option.size" :outputType="option.outputType" />
   </div>
 </template>
 
@@ -88,11 +98,32 @@ export default {
     },
     async afterRead(file) {
       // console.log('file', file);
-      const fd = new FormData();
-      fd.append('file', file.file);
-      const res = await this.$axios.post('/upload', fd);
+      //需要先进性裁剪,再进行发送请求修改
+      this.showCropper = true;
+      this.option.img = file.content;
+      // 直接修改(不经过裁剪)
+      // const fd = new FormData();
+      // fd.append('file', file.file);
+      // const res = await this.$axios.post('/upload', fd);
       // console.log('upload====>res', res);
-      this.editInfo({ head_img: res.data.data.url });
+      // this.editInfo({ head_img: res.data.data.url });
+    },
+    // 确定裁剪
+    cropper() {
+      // 获取截图的blob 数据
+      this.$refs.cropper.getCropBlob(async (data) => {
+        // console.log('data===>', data);
+        const fd = new FormData();
+        fd.append('file', data);
+        const res = await this.$axios.post('/upload', fd);
+        console.log('upload====>res', res);
+        const { statusCode, message } = res.data;
+        this.$toast(message);
+        if (statusCode === 200) {
+          this.editInfo({ head_img: res.data.data.url });
+          this.showCropper = false;
+        }
+      });
     },
     async editInfo(param) {
       const id = localStorage.getItem('user_id');
@@ -112,21 +143,36 @@ export default {
       this.showPassWord = true;
       this.password = this.userInfo.password;
     },
-    cropper() {
-      this.showCropper = false;
-    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .edit {
+  position: relative;
+  .mask {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    z-index: 9;
+  }
+  .cancle_btn {
+    position: absolute;
+    top: 0;
+    z-index: 2;
+  }
+  .confirm_btn {
+    z-index: 2;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
   .avator {
     position: relative;
     .uploader {
       position: absolute;
       top: 2px;
-      right: 40%;
+      right: 37%;
       opacity: 0;
     }
     img {
@@ -136,13 +182,6 @@ export default {
       width: 160px;
       height: 160px;
       border-radius: 50%;
-    }
-  }
-  .mask {
-    position: relative;
-    .van-button {
-      position: absolute;
-      top: 0;
     }
   }
   /deep/ .van-dialog__content {
